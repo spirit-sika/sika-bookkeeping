6ac12170f3911682ce6300c3ee4de98f50932939
小吴来哩 <2235569493@qq.com>
2024-05-07 01:05:53 +0800

    开启SpringBoot的缓存实现

--- sika-server/src/main/java/cc/sika/bookkeeping/SikaBookkeepingApplication.java
+++ sika-server/src/main/java/cc/sika/bookkeeping/SikaBookkeepingApplication.java
@@ -1,11 +1,11 @@
 package cc.sika.bookkeeping;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@MapperScan("cc.sika.bookkeeping.mapper")
+@EnableCaching
 public class SikaBookkeepingApplication {
     public static void main(String[] args) {
         SpringApplication.run(SikaBookkeepingApplication.class, args);
