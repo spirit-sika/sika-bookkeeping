2384460271d43fb70d70e3129ba18d679d5f06a8
小吴来哩 <2235569493@qq.com>
2024-05-09 16:21:26 +0800

    修改常量类命名

--- sika-commons/src/main/java/cc/sika/bookkeeping/constant/AutoFillConstant.java
+++ sika-commons/src/main/java/cc/sika/bookkeeping/constant/AutoFillConstant.java
@@ -1,9 +1,13 @@
 package cc.sika.bookkeeping.constant;
 
-public final class AutoFillMethodConstant {
+public final class AutoFillConstant {
     public static final String SET_CREATE_BY = "setCreateBy";
     public static final String SET_CREATE_TIME = "setCreateTime";
     public static final String SET_UPDATE_BY = "setUpdateBy";
     public static final String SET_UPDATE_TIME = "setUpdateTime";
     public static final String UNKNOWN = "unknown";
+    public static final String SYSTEM_GENERATE = "system";
+    public static final Byte ENABLE_STATUS = 0;
+    public static final Byte DISABLE_STATUS = 1;
+
 }
--- sika-commons/src/main/java/cc/sika/bookkeeping/constant/PublicFieldConstant.java
+++ sika-commons/src/main/java/cc/sika/bookkeeping/constant/PublicFieldConstant.java
@@ -1,8 +1,9 @@
 package cc.sika.bookkeeping.constant;
 
-public final class PublicFieldNameConstant {
+public final class PublicFieldConstant {
     public static final String UPDATE_BY = "update_by";
     public static final String UPDATE_TIME = "update_time";
     public static final String CREATE_BY = "create_by";
     public static final String CREATE_TIME = "create_time";
+
 }
