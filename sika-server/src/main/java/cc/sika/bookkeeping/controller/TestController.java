266d1f5ec4e717cb3af0058ffdf963de3ee2bf1d
小吴来哩 <2235569493@qq.com>
2024-05-09 16:24:53 +0800

    提供匿名接口与登录验证接口

--- sika-server/src/main/java/cc/sika/bookkeeping/controller/TestController.java
+++ sika-server/src/main/java/cc/sika/bookkeeping/controller/TestController.java
@@ -2,15 +2,24 @@ package cc.sika.bookkeeping.controller;
 
 import cc.sika.bookkeeping.pojo.vo.Result;
 import cn.dev33.satoken.annotation.SaCheckLogin;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
     @GetMapping("/greeting")
-    @SaCheckLogin
     public Result<String> test() {
-        return Result.success("Hello World");
+        log.info("greeting接口被请求");
+        return Result.successMessage("Hello World");
+    }
+
+    @GetMapping("/greeting-auth")
+    @SaCheckLogin
+    public Result<String> testAuth() {
+        log.info("greeting-auth接口被请求");
+        return Result.successMessage("请求已携带登录信息");
     }
 }
